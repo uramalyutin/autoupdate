c9b0ba3a46c9d775ae215a9e69f8c422ab84f45e
uramalyutin <uramalyutin@yandex.ru>
2021-10-12 17:57:24 +0300

    Создан модуль со служебными классами и основной модуль выполнения программы

---
+++ main.py
@@ -0,0 +1,5 @@
+import win32com.client as win32
+import utils
+
+comcon = utils.ComConnector()
+onec = utils.OneC(comcon, "77dln-s-a01.1plt.ru:1540")
--- utils.py
+++ utils.py
@@ -13,22 +13,22 @@ class ComConnector:
 
 
 class OneC:
-    def __init__(self, comConnector):
-        self.comConnector = comConnector
-        self.agent = self.comConnector.ConnectAgent("77dln-s-a01.1plt.ru:1540")
+    def __init__(self, comConnector, agent):
+        self.agent = comConnector.ConnectAgent(agent)
         self.clusters = self.agent.getclusters()
-        print(f'COM-объект - {self.comConnector}')
+        print(f'COM-объект - {comConnector}')
         print(f'Агент - {self.agent}')
         for cluster in self.clusters:
             print(f'Кластер - {cluster}')
             self.agent.Authenticate(cluster, "", "")
-            self.working_processes = self.agent.GetWorkingProcesses(cluster)
-            for working_process in self.working_processes:
-                print(f'Рабочий процесс {working_process.HostName + ":" + str(working_process.MainPort)}')
+            self.workingProcesses = self.agent.GetWorkingProcesses(cluster)
+            for workingProcess in self.workingProcesses:
+                print(f'Рабочий процесс {workingProcess.HostName + ":" + str(workingProcess.MainPort)}')
+
 
 def main():
     com_connector = ComConnector()
-    onec = OneC(com_connector)
+    onec = OneC(com_connector, "77dln-s-a01.1plt.ru:1540")
 
 
 if __name__ == "__main__":
