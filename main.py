c36200d3e7c670e870f19d5db9791ec4237df87e
uramalyutin <uramalyutin@yandex.ru>
2021-10-13 18:00:53 +0300

    Реализовано получение списка баз

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import utils
 comcon = utils.ComConnector()
 if comcon is not None:
     onec = utils.OneC(comcon, "77dln-s-a01.1plt.ru:1540")
+    iBases = onec.getIB()
+    print(f'Обнаружены информационные базы:')
+    for ib in iBases:
+        print(f'    {ib.name}')
 else:
     print("Не смогли создать COM-соединение")
--- utils.py
+++ utils.py
@@ -22,12 +22,26 @@ class OneC:
         self.clusters = self.agent.getclusters()
         for cluster in self.clusters:
             print(f'Получили кластер - {cluster.HostName}:{cluster.MainPort}')
+            # Если в кластере есть Администраторы кластера, нужно передавать логин и пароль для авторизации
             self.agent.Authenticate(cluster, "", "")
             self.workingProcesses = self.agent.GetWorkingProcesses(cluster)
-            print(f'Получили рабочие процессы:')
+            print(f'Получили рабочие процессы кластера:')
             for workingProcess in self.workingProcesses:
                 print(f'    {workingProcess.HostName + ":" + str(workingProcess.MainPort)}')
 
+
+    def getClusters(self):
+        return self.agent.getclusters()
+
+
+    def getWorkingProcess(self):
+        pass
+
+
+    def getIB(self):
+        for cluster in self.clusters:
+            infoBases = self.agent.getInfoBases(cluster)
+        return infoBases
 
 def main():
     com_connector = ComConnector()
