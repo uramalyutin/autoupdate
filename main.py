28be809b26946345f7bd1fd68bb7de355c56246a
uramalyutin <uramalyutin@yandex.ru>
2022-05-23 16:59:54 +0300

    Наверное, всё-таки нужно закончить проект

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-
 import utils
+
 
 comcon = utils.ComConnector()
 if comcon is not None:
-    onec = utils.OneC(comcon, "77dln-s-a01.1plt.ru:1540")
-    iBases = onec.getIB()
-    print(f'Обнаружены информационные базы:')
-    for ib in iBases:
-        print(f'    {ib.name}')
+    onec = utils.OneC(comcon)
 else:
     print("Не смогли создать COM-соединение")
--- utils.py
+++ utils.py
@@ -1,46 +1,66 @@
-
 import win32com.client as win32
 
 
 class ComConnector:
-
     _instance = None
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = win32.Dispatch("V83.COMConnector")
+            cls._instance = win32.Dispatch("V83.COMConnector")  # 8.3.21.1302
             print(f'Создали COM-объект - {cls._instance}')
         return cls._instance
 
 
 class OneC:
     connector = None
-    def __init__(self, comConnector, server):
-        if OneC.connector == None:
+
+    def __init__(self, comConnector):
+        if OneC.connector is None:
             OneC.connector = comConnector
+        self.agent = None
+        self.clusters = None
+        self.workingProcesses = None
+        self.infoBases = None
+
+    def getAgent(self, server):
         self.agent = OneC.connector.ConnectAgent(server)
-        print(f'Подключились к агенту - {self.agent.ConnectionString}')
+        return self.agent
 
+    def getClusters(self):
         self.clusters = self.agent.getclusters()
-        for cluster in self.clusters:
-            print(f'Получили кластер - {cluster.HostName}:{cluster.MainPort}')
-            # Если в кластере есть Администраторы кластера, нужно передавать логин и пароль для авторизации
-            self.agent.Authenticate(cluster, "", "")
-            self.workingProcesses = self.agent.GetWorkingProcesses(cluster)
-            print(f'Получили рабочие процессы кластера:')
-            for workingProcess in self.workingProcesses:
-                print(f'    {workingProcess.HostName + ":" + str(workingProcess.MainPort)}')
+        return self.clusters
 
+    def getWorkingProcesses(self, cluster, clusterLogin, clusterPass):
+        # Если в кластере есть Администраторы кластера, нужно передавать логин и пароль для авторизации
+        self.agent.Authenticate(cluster, clusterLogin, clusterPass)
+        self.workingProcesses = self.agent.GetWorkingProcesses(cluster)
+        return self.workingProcesses
 
-    def getIB(self):
+    def getInfoBases(self, cluster, clusterLogin, clusterPass):
+        # Если в кластере есть Администраторы кластера, нужно передавать логин и пароль для авторизации
+        self.agent.Authenticate(cluster, clusterLogin, clusterPass)
         for cluster in self.clusters:
-            infoBases = self.agent.getInfoBases(cluster)
-        return infoBases
+            self.infoBases = self.agent.getInfoBases(cluster)
+        return self.infoBases
 
 
 def main():
     com_connector = ComConnector()
-    onec = OneC(com_connector, "77dln-s-a01.1plt.ru:1540")
+    onec = OneC(com_connector)
+    onec_agent = onec.getAgent("a00-77dln-a30.1plt.ru:1540")
+    print(f'Подключились к агенту - {onec_agent.ConnectionString}')
+    onec_clusters = onec.getClusters()
+    print(f'Получили кластеры сервера:')
+    for onec_cluster in onec_clusters:
+        print(f'    {onec_cluster.HostName}:{onec_cluster.MainPort}')
+        onec_workingProcesses = onec.getWorkingProcesses(onec_cluster, "Yurij.Malyutin", "pass")
+        print(f'Получили рабочие процессы кластера:')
+        for onec_workingProcess in onec_workingProcesses:
+            print(f'    {onec_workingProcess.HostName + ":" + str(onec_workingProcess.MainPort)}')
+        onec_infoBases = onec.getInfoBases(onec_cluster, "Yurij.Malyutin", "pass")
+        print(f'Получили список баз:')
+        for onec_infoBase in onec_infoBases:
+            print(f'    {onec_infoBase.Name}')
 
 
 if __name__ == "__main__":
