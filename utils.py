50e8e989bdd50c55999147187fec8df77fd89b6e
uramalyutin <uramalyutin@yandex.ru>
2024-12-19 18:22:57 +0300

    Добавлено логирование в модуль utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import win32com.client as win32
+import logging
 
 
 class ComConnector:
@@ -6,33 +7,56 @@ class ComConnector:
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = win32.Dispatch("V83.COMConnector")  # 8.3.21.1302
+            cls._instance = win32.Dispatch("V83.COMConnector")  # 8.3.23.2040
+        logger.info(f'Создали COM-объект - {cls._instance}')
         return cls._instance
 
 
 class OneC:
     comConnector = None
-
+    
     def __init__(self, comConnector, onecServer, mainPort, clusterLogin, clusterPass):
-        if OneC.comConnector is None:
-            OneC.comConnector = comConnector
-        self.agent = OneC.comConnector.ConnectAgent(onecServer)
-        self.clusters = self.agent.getclusters()
-        # На одном сервере может быть несколько кластеров, поэтому нужно найти нужный нам кластер
-        workCluster = None
-        for cluster in self.clusters:
-            if cluster.MainPort == int(mainPort):
-                workCluster = cluster
-                break
-        if workCluster is not None:
-            # Если в кластере есть Администраторы кластера, нужно авторизоваться как Администратор кластера,
-            # чтобы получить рабочие процессы и информационные базы
-            self.agent.Authenticate(workCluster, clusterLogin, clusterPass)
-            self.workingProcesses = self.agent.GetWorkingProcesses(workCluster)
-            self.infoBases = self.agent.getInfoBases(workCluster)
-        else:
-            self.workingProcesses = None
-            self.infoBases = None
+        try:
+            logger.info(f'Начало инициализации кластера {onecServer}')
+            if OneC.comConnector is None:
+                OneC.comConnector = comConnector
+            self.agent = OneC.comConnector.ConnectAgent(onecServer)
+            logger.info(f'Подключились к агенту - {self.agent.ConnectionString}')
+            self.clusters = self.agent.getclusters()
+            logger.info(f'Получили кластеры сервера')
+            if logger.level == logging.DEBUG:
+                for cluster in self.clusters:
+                    logger.debug(f'    {cluster.HostName + ":" + str(cluster.MainPort)}')
+            # На одном сервере может быть несколько кластеров, поэтому нужно найти нужный нам кластер
+            workCluster = None
+            for cluster in self.clusters:
+                logger.debug(f'Ищем кластер с портом {cluster.MainPort}')
+                if cluster.MainPort == int(mainPort):
+                    workCluster = cluster
+                    logger.debug(f'Получили кластер {cluster.HostName}:{cluster.MainPort}')
+                    break
+            if workCluster is not None:
+                # Если в кластере есть Администраторы кластера, нужно авторизоваться как Администратор кластера,
+                # чтобы получить рабочие процессы и информационные базы
+                self.agent.Authenticate(workCluster, clusterLogin, clusterPass)
+                logger.info(f"Выполнили аутентификацию на кластере")
+                self.workingProcesses = self.agent.GetWorkingProcesses(workCluster)
+                logger.info(f'Получили рабочие процессы на кластера')
+                if logger.level == logging.DEBUG:
+                    for workingProcess in self.workingProcesses:
+                        logger.debug(f'    {workingProcess.HostName + ":" + str(workingProcess.MainPort)}')
+                self.infoBases = self.agent.getInfoBases(workCluster)
+                logger.info(f'Получили список баз на кластере')
+                if logger.level == logging.DEBUG:
+                    for infoBase in self.infoBases:
+                        logger.debug(f'    {infoBase.Name}')
+
+            else:
+                self.workingProcesses = None
+                self.infoBases = None
+        except:
+            logger.exception('В процессе инициализации кластера произошла ошибка')
+        logger.info(f'Завершение инициализации кластера {onecServer}')
 
     def getAgent(self):
         return self.agent
@@ -49,22 +73,30 @@ class OneC:
 
 def main():
     com_connector = ComConnector()
-    print(f'Создали COM-объект - {com_connector}')
     onec = OneC(com_connector, "a00-77dln-a30.1plt.ru:1540", "1541", "Yurij.Malyutin", "pass")
     onec_agent = onec.getAgent()
-    print(f'Подключились к агенту - {onec_agent.ConnectionString}')
     onec_clusters = onec.getClusters()
-    print(f'Получили кластеры сервера:')
     for onec_cluster in onec_clusters:
-        print(f'    {onec_cluster.HostName}:{onec_cluster.MainPort}')
         onec_workingProcesses = onec.getWorkingProcesses()
-        print(f'Получили рабочие процессы кластера:')
-        for onec_workingProcess in onec_workingProcesses:
-            print(f'    {onec_workingProcess.HostName + ":" + str(onec_workingProcess.MainPort)}')
         onec_infoBases = onec.getInfoBases()
-        print(f'Получили список баз:')
-        for onec_infoBase in onec_infoBases:
-            print(f'    {onec_infoBase.Name}')
+
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(
+    filename = "updater_log.log", mode="w", encoding = 'utf-8'
+)
+console_handler = logging.StreamHandler()
+file_format = logging.Formatter(
+    '%(asctime)s;%(name)s;%(levelname)s;%(message)s'
+)
+console_format = logging.Formatter(
+    "[%(asctime)s];%(name)s;%(levelname)s;%(message)s","%H:%M:%S"
+)
+file_handler.setFormatter(file_format)
+console_handler.setFormatter(console_format)
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 
 if __name__ == "__main__":
