d1619e3854b5587c9f3b2ed9cbb4fd9401a5672f
uramalyutin <uramalyutin@yandex.ru>
2024-12-26 18:04:57 +0300

    Подключение к кластеру и рабочим процессам. Рефакторинг.

--- main.py
+++ main.py
@@ -1,39 +1,81 @@
 import utils
 import yaml
+import datetime
 
 
 def getSettings(filename):
+    settings = None
     with open(filename) as stream:
         try:
             settings = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            logger.exception(f'Не смогли прочитать настройки из файла, по причине: {exc}')
+            return settings
+        else:
             logger.info(f'Прочитали настройки')
             logger.debug(f'Прочитанные настройки:')
             logger.debug(f'{settings}')
-        except yaml.YAMLError as exc:
-            logger.exception(f'Не смогли прочитать настройки из файла {exc}')
-    return settings
+            return settings
 
 
-#Подключим логирование
+# Подключим логирование
 logger = utils.getLogger("main", "w")
 
-#Прочитаем настройки из updater_settings.yaml
+# Получим COM-коннектор
+connector = utils.ComConnector()
+
+# Прочитаем настройки из updater_settings.yaml
 settings = getSettings("updater_settings.yaml")
 
-#Создадим COM-коннектор
-com_connector = utils.ComConnector()
+clusters = settings.get('Clusters')
+for cluster in clusters:    
+    # Получим объект для работы с кластером 1С
+    onec = utils.OneC_Server(cluster, connector)
+    
+    # Получим агента сервера 1С
+    onec_agent = onec.getAgent()
+    
+    # Получим кластеры 
+    onec_clusters = onec.getAllClusters()
+    
+    # Получим кластер с нужным нам портом
+    onec_workCluster = onec.getClusterWithPort(cluster.get('ClusterPort'))
+    
+    # Получим рабочие процессы сервера
+    if onec_workCluster is not None:
+        onec_workingProcesses = onec.getWorkingProcesses()
+        
+    # Получим центральный сервер кластера
+    onec_centralServer = onec.getCentralServer()
+        
+    # Сформируем строку подключения к рабочему процессу, обслуживающему базы.
+    # Это будет процесс с HostName, равным HostName центрального кластера, 
+    # и портом, равным MainPort этого рабочего процесса
+    for wp in onec_workingProcesses:
+        if wp.HostName == onec_centralServer.HostName:
+            workingAddress = wp.HostName + ':' + str(wp.MainPort)
+            break
 
-if com_connector is not None:
-    #Обойдём базы, с которыми будем работать
-    infobases = settings.get('Infobases')
+    # Подключимся к рабочему процессц, обслуживающему соединения с базами.
+    workingProcessConnect = connector.ConnectWorkingProcess(workingAddress)
+    
+    # Получим список информационных баз кластера
+    onec_infoBases = onec.getInfoBases()
+    
+    # Обойдём базы, и найдём с которыми будем работать
+    infobases = cluster.get('Infobases')
     for infobase in infobases:
-        #Получим объект для работы с кластером
-        onec = utils.OneC_Cluster(com_connector, infobase)
-        onec_agent = onec.getAgent()
-        onec_clusters = onec.getClusters()
-        onec_workCluster = onec.getClusterWithPort(infobase.get('ServerPort'))
-        if onec_workCluster is not None:
-            onec_workingProcesses = onec.getWorkingProcesses()
-            onec_infoBases = onec.getInfoBases()
-        if onec_infoBases is not None:
-            onec_infoBase = onec.getInfoBase()
+        ib = workingProcessConnect.CreateInfoBaseInfo()
+        ib.name = infobase.get('InfobaseName')
+        ib.DeniedFrom = datetime.datetime.now()
+        lockTime = int(infobase.get('LockTime'))
+        ib.DeniedTo = ib.DeniedFrom + datetime.timedelta(minutes = lockTime)
+        ib.PermissionCode = "123456"
+        ib.SessionsDenied = True
+        workingProcessConnect.UpdateInfoBase(ib)
+        
+    #for IBSettings in infobases:
+        
+
+    # if onec_infoBases is not None:
+    #     onec_infoBase = onec.getInfoBase()
--- utils.py
+++ utils.py
@@ -6,113 +6,159 @@ class ComConnector:
     _instance = None
 
     def __new__(cls):
-        if cls._instance is None:
-            try:
-                cls._instance = win32.Dispatch("V83.COMConnector")  # 8.3.23.2040
-                logger.info(f'Создали COM-объект - {cls._instance}')
-                return cls._instance
-            except:
-                logger.exception('В процессе создания COM-объекта произошла ошибка')
-                return cls._instance
-
-
-class OneC_Cluster:
-    _comConnector = None
-    
-    def __init__(self, comConnector, ibsettings):
-        logger.info(f'Начало инициализации кластера {ibsettings.get('ServerName')}')
+        if cls._instance is not None:
+            return cls._instance
         
-        #В общем случае COM-коннектор может быть только одной версии, поэтому сохраним его для всего класса
-        if OneC_Cluster._comConnector is None:
-            OneC_Cluster._comConnector = comConnector
+        try:
+            cls._instance = win32.Dispatch("V83.COMConnector")  # 8.3.23.2040
+            #logger.info(f'Создали COM-объект - {cls._instance}')
+        except:
+            logger.exception('В процессе создания COM-объекта произошла ошибка')
+            return cls._instance
+        else:
+            logger.info(f'Создали COM-объект - {cls._instance}')
+            return cls._instance
 
-        self.onecServer = ibsettings.get('ServerName')
-        self.clusterLogin = ibsettings.get('ClusterLogin')
-        self.clusterPass = ibsettings.get('ClusterPassword')
+
+class OneC_Server:
+    _comConnector = None
+
+    def __new__(cls, settings, comConnector):
+        # В общем случае COM-коннектор может быть только одной версии, поэтому сохраним его для всего класса
+        if cls._comConnector is None:
+            # Добавим COM-коннектор
+            cls._comConnector = comConnector
+        return super().__new__(cls)
+    
+    def __init__(self, settings, comConnector):
+        self.serverName = settings.get('ServerName')
+        self.serverPort = settings.get('ServerPort')
+        self.clusterPort = settings.get('ClusterPort')
+        self.clusterLogin = settings.get('ClusterLogin')
+        self.clusterPassword = settings.get('ClusterPassword')
+        self.IBName = settings.get('InfoBaseName')
+        self.IBLogin = settings.get('InfobaseLogin')
+        self.IBPass = settings.get('InfobasePassword')
+ 
         self.agent = None
         self.clusters = None
         self.workCluster = None
         self.workingProcesses = None
+        self.workingServers = None
+        self.centralServer = None
         self.infoBases = None
  
-        logger.info(f'Завершение инициализации кластера {ibsettings.get('ServerName')}')
+        logger.info(f'Проинициализировали объект для работы с кластером')
 
-    def getAgent(self):
-        if self.agent is None:
-            #Подключаеся к агенту
-            try:
-                logger.debug(f'Подключаемся к агенту - {self.onecServer}')
-                self.agent = OneC_Cluster._comConnector.ConnectAgent(self.onecServer)
-            except:
-                logger.exception('В процессе подключения к агенту произошла ошибка')        
+    def getAgent(self): 
+        '''Подключаеся к агенту'''
+        try:
+            self.agent = OneC_Server._comConnector.ConnectAgent(self.serverName)
+        except:
+            logger.exception('В процессе подключения к агенту произошла ошибка')        
+        else:
+            logger.info(f'Подключились к агенту - {self.serverName}')
         return self.agent
 
-    def getClusters(self):
-        if self.clusters is None:
-            #Получаем кластеры сервера
-            try:
-                logger.debug(f'Получаем кластеры сервера')
-                self.clusters = self.agent.getclusters()
-                if logger.level == logging.DEBUG:
-                    logger.debug(f'Получили кластеры:')
-                    for cluster in self.clusters:
-                        logger.debug(f'    {cluster.HostName + ":" + str(cluster.MainPort)}')
-            except:
-                logger.exception('В процессе получения кластеров произошла ошибка')
-        return self.clusters
-
-    def getClusterWithPort(self, port):
-        if self.workCluster is None:
-            # На одном сервере может быть несколько кластеров, поэтому нужно найти кластер с нужным портом
-            try:
-                logger.debug(f'Ищем кластер с портом {port}')
+    def getAllClusters(self):
+        '''Получаем кластеры сервера'''
+        try:
+            self.clusters = self.agent.getclusters()
+        except:
+            logger.exception('В процессе получения кластеров произошла ошибка')
+        else:
+            logger.info(f'Получили кластеры сервера')
+            if logger.level == logging.DEBUG:
                 for cluster in self.clusters:
-                    if cluster.MainPort == int(port):
-                        self.workCluster = cluster
-                        logger.debug(f'Получили кластер {cluster.HostName}:{cluster.MainPort}')
-                        break
-            except:
-                logger.exception(f'В процессе поиска кластера с портом {port} произошла ошибка')
-            
-            # Если в кластере есть Администраторы кластера, нужно авторизоваться как Администратор кластера,
-            # чтобы получить рабочие процессы и информационные базы
-            try:
-                logger.debug(f"Выполняем аутентификацию на кластере")
-                self.agent.Authenticate(self.workCluster, self.clusterLogin, self.clusterPass)
-            except:
-                logger.exception(f'В процессе аутентификации на кластере произошла ошибка')
+                    logger.debug(f'    {cluster.HostName + ":" + str(cluster.MainPort)}')
+        return self.clusters
+    
+    def getClusterWithPort(self, port):
+        '''На одном сервере может быть несколько кластеров, поэтому нужно найти кластер с нужным портом'''
+        try:
+            for cluster in self.clusters:
+                if cluster.MainPort == int(port):
+                    self.workCluster = cluster
+                    break
+        except:
+            logger.exception(f'В процессе поиска кластера с портом {port} произошла ошибка')
+        else:
+            logger.info(f'Получили кластер {cluster.HostName}:{cluster.MainPort}')
+    
+        # Если в кластере есть Администраторы кластера, нужно авторизоваться как Администратор кластера,
+        # чтобы получить рабочие процессы и информационные базы
+        try:
+            self.agent.Authenticate(self.workCluster, self.clusterLogin, self.clusterPassword)
+        except:
+            logger.exception(f'В процессе аутентификации на кластере произошла ошибка')
+        else:
+            logger.info(f"Выполнили аутентификацию на кластере")
         return self.workCluster
 
     def getWorkingProcesses(self):
-        if self.workingProcesses is None:
-            #Получим рабочие процессы кластера
-            try:
-                logger.debug(f'Получим рабочие процессы на кластере')
-                self.workingProcesses = self.agent.GetWorkingProcesses(self.workCluster)
-                if logger.level == logging.DEBUG:
-                    logger.debug(f'Получиили рабочие процессы:')
-                    for workingProcess in self.workingProcesses:
-                        logger.debug(f'    {workingProcess.HostName + ":" + str(workingProcess.MainPort)}')
-            except:
-                logger.exception(f'В процессе получения рабочих процессов на кластере произошла ошибка')
+        #Получим рабочие процессы кластера
+        try:
+            self.workingProcesses = self.agent.GetWorkingProcesses(self.workCluster)
+        except:
+            logger.exception(f'В процессе получения рабочих процессов на кластере произошла ошибка')
+        else:
+            logger.info(f'Получили рабочие процессы на кластере')
+            if logger.level == logging.DEBUG:
+                for workingProcess in self.workingProcesses:
+                    logger.debug(f'    {workingProcess.HostName + ":" + str(workingProcess.MainPort)}')
         return self.workingProcesses
 
+    def getWorkingServers(self):
+        #Получим рабочие серверы кластера
+        try:
+            self.workingServers = self.agent.GetWorkingServers(self.workCluster)
+        except:
+            logger.exception(f'В процессе получения рабочих серверов на кластере произошла ошибка')
+        else:
+            logger.info(f'Получили рабочие серверы на кластере')
+            if logger.level == logging.DEBUG:
+                for workingServers in self.workingServers:
+                    logger.debug(f'    {workingServers.HostName + ":" + str(workingServers.MainPort)}')
+        return self.workingServers
+
+    def getCentralServer(self):
+        #Получим центральный сервер кластера
+        if self.workingServers is None:
+            self.getWorkingServers()
+        try:
+            for ws in self.workingServers:
+                if ws.Name == 'Центральный сервер':
+                    self.centralServer = ws
+                    break
+        except:
+            logger.exception(f'В процессе получения центрального сервера кластера произошла ошибка')
+        else:
+            logger.info(f'Получили центральный сервер кластера')
+            if logger.level == logging.DEBUG:
+                logger.debug(f'    {self.centralServer.HostName + ":" + str(self.centralServer.MainPort)}')
+        return self.centralServer
+
     def getInfoBases(self):
-        if self.infoBases is None:
-            #Получим информационные базы кластера
-            try:
-                logger.debug(f'Получим список информационных баз на кластере')
-                self.infoBases = self.agent.getInfoBases(self.workCluster)
-                if logger.level == logging.DEBUG:
-                    logger.debug(f'Получиили информационные базы:')
-                    for infoBase in self.infoBases:
-                        logger.debug(f'    {infoBase.Name}')
-            except:
-                logger.exception(f'В процессе получения информационных баз на кластере произошла ошибка')        
+        #Получим информационные базы кластера
+        try:
+            self.infoBases = self.agent.getInfoBases(self.workCluster)
+        except:
+            logger.exception(f'В процессе получения информационных баз на кластере произошла ошибка')
+        else:
+            logger.info(f'Получли список информационных баз на кластере')
+            if logger.level == logging.DEBUG:
+                for infoBase in self.infoBases:
+                    logger.debug(f'    {infoBase.Name}')
         return self.infoBases
 
-    def getInfoBase(self):
-        pass
+    def getInfoBase(self, IBName):
+        for ib in self.infoBases:
+            if IBName == ib.Name:
+                logger.info(f'Нашли базу {ib.Name}')
+                return ib
+            else:
+                continue
+        return None
 
 
 def getLogger(loggerName, loggerMode):
